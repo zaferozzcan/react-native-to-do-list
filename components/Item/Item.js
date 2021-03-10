11d599f3077c139dccafe969c72a4a8fc19cb8bd
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-10 16:43:45 -0500

    complete item component

--- components/Item/Item.js
+++ components/Item/Item.js
@@ -1,23 +1,51 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function Item(props) {
+const Task = (props) => {
   return (
-    <View>
-      <View style={styles.itemLeft}></View>
-      <Text>{props.text}</Text>
-      <View style={styles.circle}></View>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
     </View>
   );
-}
-
-const styles = {
-  itemLeft: {
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    backgroundColor: "lightblue",
-  },
-  text: { fontSize: 20, fontWeigth: "bold" },
-  circle: { width: 20, height: 20, borderRadius: 50 },
 };
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Task;
